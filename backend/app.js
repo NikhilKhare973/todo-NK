51174298b087f4fac16685da5fe8b270162d6b1f
Nikhil khare <143277827+NikhilKhare973@users.noreply.github.com>
2024-08-12 10:06:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose'); 
-const app = express();
+// const app = express();
 const cors = require("cors");
 require("./conn/conn"); 
 const path = require("path");
