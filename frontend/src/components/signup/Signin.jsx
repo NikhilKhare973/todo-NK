74307ad5c56e74de091467f2343a8f38cd0c9bc9
Nikhil khare <143277827+NikhilKhare973@users.noreply.github.com>
2024-08-13 20:04:09 +0530

    Update Signin.jsx

--- frontend/src/components/signup/Signin.jsx
+++ frontend/src/components/signup/Signin.jsx
@@ -25,7 +25,7 @@ const SignIn= () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `https://todo-nk-api.vercel.app//api/v1/signin`,
+        `https://todo-nk-api.vercel.app/api/v1/signin`,
         Inputs
       );
       console.log(response.data);
