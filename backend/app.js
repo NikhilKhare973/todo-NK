f8bc6410e9ebd6e3daa31bbc9981f57e32c2bcb2
Nikhil khare <143277827+NikhilKhare973@users.noreply.github.com>
2024-08-13 09:27:12 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const auth = require("./routes/auth");
 const list = require("./routes/list");
 app.use(express.json());
 app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
+// app.options("", cors(corsConfig));
 
 const DB = 'mongodb+srv://nikhilkhare212:Nikhil123@cluster0.gv4ygej.mongodb.net/';
 
@@ -31,11 +31,11 @@ app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
 
-const corsConfig =  {
-        origin: ["*"],
-        methods: ["GET","POST", "PUT", "DELETE"],
-        credentials: true
-    };
+// const corsConfig =  {
+//         origin: ["*"],
+//         methods: ["GET","POST", "PUT", "DELETE"],
+//         credentials: true
+//     };
     app.use(express.json());
     
     app.get("/", (req, res) => {
